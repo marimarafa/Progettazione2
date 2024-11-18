5e8ea59af1e2a38838ab24375f1ebac9c00d628f
marimarafa <arafamarim166@gmail.com>
2024-11-18 13:28:03 +0100

    ..

--- ProgettoSQL/dataBase.py
+++ ProgettoSQL/dataBase.py
@@ -1,20 +1,37 @@
 import DBconnection as db
+import sys
 
 cursor = db.connection.cursor()
+def PrintQuery():
+    rows = cursor.fetchall()
+    count = 0
+    for row in rows:
+        count +=1
+        print(row)
+    print("\tTotal rows : " , count)
 
-query = "select * from persona where posizione = 'Ricercatore'"
-query2 = "select * from progetto"
-
-cursor.execute(query)
-cursor.execute(query2)
-
-#recupera i risultati
-rows = cursor.fetchall()
-count = 0
-for row in rows:
-    count +=1
-    print(row)
-print("\tTotal rows : " , count)
+while True :
+    op = print("\nScegliere query da eseguire:\n1)scegli la tabella \n2)scegli la tabella con condizione \n3)Stampa query \n4)esci ")
+    num_op = input("--> ")
+    if num_op == "1":
+        tabella = input("Inserisci nome della tabella -> ")
+        query1 = f"select * from {tabella} "
+        cursor.execute(query1)
+        PrintQuery()
+        
+    elif num_op == "2":
+        tabella = input("Inserisci nome della tabella -> ")
+        condizione = input("inserisci la condizione ->  ")
+        query2 = f"select * from {tabella} where {condizione} "
+        cursor.execute(query2)
+        PrintQuery()
+       
+    elif num_op == "3":
+        query3 = "select * from AttivitaProgetto where tipo = 'Ricerca e Sviluppo' and oredurata = 6"
+        cursor.execute(query3)
+        PrintQuery()
+    else:
+        sys.exit()
 
 
 
